b4700bd7c442196a174bb63ff80873f630e36c70
armanbhardwaj <44959827+armanbhardwaj@users.noreply.github.com>
2018-11-19 15:21:07 +0530

    5th Module Completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+
 let quizData = require('./quiz_data.json')
 
 
@@ -11,9 +13,11 @@ class Quiz extends Component{
     };
     render()
     {
+        const isQuizEnd = true;
         return(<div>
+                { isQuizEnd ? <QuizEnd /> :
                 <QuizQuestion quiz_question ={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-
+                }
         </div>);
     }
 }
